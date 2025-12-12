c656904ac417eb252dbde8812cde1c1bc2e6d920
Pranav Nanegaonkar <nanegaonkarpranav68@gmail.com>
2025-12-12 17:23:09 +0530

    Update AuthLayout component to include padding for improved layout consistency

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex-center min-h-screen bg-auth-light dark:bg-auth-dark bg-cover bg-center bg-no-repeat">
+    <main className="flex-center min-h-screen px-4 py-10 bg-auth-light dark:bg-auth-dark bg-cover bg-center bg-no-repeat">
       <section className="light-border background-light800_dark200 shadow-light100_dark100 min-w-[90%] rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8 ">
         <div className="flex-between gap-2">
           <div className="space-y-2.5">
